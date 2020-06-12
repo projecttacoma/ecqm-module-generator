17beadae2a15c9fe413bceba80fd87275a3b7f1a
Cassidy King <cassidyking17@gmail.com>
2020-06-12 09:44:27 -0400

    added toJSON methods

--- states.js
+++ states.js
@@ -9,6 +9,12 @@ class InitialState extends BaseState {
         super(name);
         this.type = 'Initial';
     }
+    toJSON() {
+        return {
+            "type": this.type,
+            "direct_transition": this.name
+        }
+    }
 }
 class EncounterState extends BaseState {
     constructor(name, encounter_class, codes) {
@@ -17,11 +23,25 @@ class EncounterState extends BaseState {
         this.encounter_class = encounter_class;
         this.codes = codes;
     }
+    toJSON() {
+        return {
+            "type": this.type,
+            "encounter_class":this.encounter_class,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
+    }
 }
 class EncounterEndState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'EncounterEnd';
+    }
+    toJSON() {
+        return {
+            "type": this.type,
+            "direct_transition": this.name
+        }
     }
 }
 class ConditionOnsetState extends BaseState {
@@ -31,11 +51,25 @@ class ConditionOnsetState extends BaseState {
         this.target_encounter = target_encounter;
         this.codes = codes;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "target_encounter":this.target_encounter,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
+    }
 }
 class ConditionEndState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'ConditionEnd';
+    }
+    toJSON() {
+        return {
+            "type":this.type,
+            "direct_transition":this.name
+        }
     }
 }
 class AlleryOnsetState extends BaseState {
@@ -45,11 +79,25 @@ class AlleryOnsetState extends BaseState {
         this.target_encounter = target_encounter;
         this.codes = codes;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "target_encounter":this.target_encounter,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
+    }
 }
 class AllergyEndState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'AllergyEnd';
+    }
+    toJSON() {
+        return {
+            "type":this.type,
+            "direct_transition":this.name
+        }
     }
 }
 class MedicationOrderState extends BaseState {
@@ -58,11 +106,24 @@ class MedicationOrderState extends BaseState {
         this.type = 'MedicationOrder';
         this.codes = codes;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
+    }
 }
 class MedicationEndState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'MedicationEnd';
+    }
+    toJSON() {
+        return {
+            "type":this.type,
+            "direct_transition":this.name
+        }
     }
 }
 class CarePlanStartState extends BaseState {
@@ -71,21 +132,40 @@ class CarePlanStartState extends BaseState {
         this.type = 'CarePlanStart';
         this.codes = codes;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
+    }
 }
 class CarePlanEndState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'CarePlanEnd';
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "direct_transition":this.name
+        }
+    }
 }
 class ProcedureState extends BaseState {
-    constructor(name, codes, duration, unit) {
+    constructor(name, codes, duration) {
         super(name);
         this.type = 'Procedure';
         this.codes = codes;
-        //these are in a object 'duration'
         this.duration = duration;
-        this.unit=unit;
+    }
+    toJSON() {
+        return {
+            "type": this.type,
+            "codes": this.codes,
+            "duration": this.duration,
+            "direct_transition": this.name
+        }
     }
 }
 class ImagingStudyState extends BaseState {
@@ -95,6 +175,14 @@ class ImagingStudyState extends BaseState {
         this.procedure_code = procedure_code;
         this.series = series;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "procedure_code":this.procedure_code,
+            "series":this.series,
+            "direct_transition":this.name
+        }
+    }
 }
 class DeviceState extends BaseState {
     constructor(name, code) {
@@ -102,11 +190,24 @@ class DeviceState extends BaseState {
         this.type = 'Device';
         this.code = code;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "code":this.code,
+            "direct_transition":this.name
+        }
+    }
 }
 class DeviceEndState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'DeviceEnd';
+    }
+    toJSON() {
+        return {
+            "type":this.type,
+            "direct_transition":this.name
+        }
     }
 }
 class SupplyListState extends BaseState {
@@ -115,6 +216,13 @@ class SupplyListState extends BaseState {
         this.type = 'SupplyList';
         this.supplies = supplies;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "supplies":this.supplies,
+            "direct_transition":this.name
+        }
+    }
 }
 class VitalSignState extends BaseState {
     constructor(name, vital_sign, unit) {
@@ -122,6 +230,14 @@ class VitalSignState extends BaseState {
         this.type = 'VitalSign';
         this.vital_sign = vital_sign;
         this.unit = unit;
+    }
+    toJSON() {
+        return {
+            "type":this.type,
+            "vital_sign":this.vital_sign,
+            "unit":this.unit,
+            "direct_transition":this.name
+        }
     }
 }
 class ObservationState extends BaseState {
@@ -132,6 +248,15 @@ class ObservationState extends BaseState {
         this.unit = unit;
         this.codes = codes;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "category":this.category,
+            "unit":this.unit,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
+    }
 }
 class MultiObservationState extends BaseState {
     constructor(name, category, number_of_observations, codes) {
@@ -141,13 +266,30 @@ class MultiObservationState extends BaseState {
         this.number_of_observations = number_of_observations;
         this.codes = codes;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "category":this.category,
+            "number_of_observations":this.number_of_observations,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
+    }
 }
 class DiagnosticReportState extends BaseState {
     constructor(name, number_of_observations, codes) {
         super(name);
-        this.type = 'DiagnosticReport';
-        this.number_of_observations = number_of_observations;
+        this.type = 'DiagnosticReport',
+        this.number_of_observations = number_of_observations,
         this.codes = codes;
+    }
+    toJSON() {
+        return {
+            "type":this.type,
+            "number_of_observations":this.number_of_observations,
+            "codes":this.codes,
+            "direct_transition":this.name
+        }
     }
 }
 class SymptomState extends BaseState {
@@ -158,17 +300,38 @@ class SymptomState extends BaseState {
         this.cause = cause;
         this.probability = probability;
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "symptom":this.symptom,
+            "cause":this.cause,
+            "probability":this.probability,
+            "direct_transition":this.name
+        }
+    }
 }
 class DeathState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'Death';
     }
+    toJSON() {
+        return {
+            "type":this.type,
+            "direct_transition":this.name
+        }
+    }
 }
 class TerminalState extends BaseState {
     constructor(name) {
         super(name);
         this.type = 'Terminal';
+    }
+    toJSON() {
+        return {
+            "type":this.type,
+            "direct_transition":this.name
+        }
     }
 }
 
