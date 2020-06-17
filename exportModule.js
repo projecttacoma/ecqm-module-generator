05ba3ef1fd913b9f7ddf085a9b2efd6ee138ff3c
Cassidy King <cassidyking17@gmail.com>
2020-06-17 10:02:01 -0400

    added a try/catch to handle errors

---
+++ exportModule.js
@@ -0,0 +1,29 @@
+const data = require('./response.json');
+const loadData = require('./GetDataType.js');
+const states = require('./states.js');
+const stateLookup = require('./stateMap.js');
+function exportModule(data) {
+    const dataTypes = loadData();
+    const moduleJSON = {
+        "name": data.library.identifier.id,
+        "remarks": [],
+        "states": {
+            "Initial": new states.InitialState('Initial').toJSON(),
+            "Terminal": new states.TerminalState().toJSON()
+        }
+    };
+    dataTypes.forEach(function (temp, i) {
+        try {
+            const map = stateLookup[temp];
+            const stateName = `${temp.substring(21)}_${i}`;
+            const state = new map(stateName, [], {})
+            moduleJSON.states[stateName] = state.toJSON();
+        }
+        catch{
+            return;
+        }
+    })
+    return moduleJSON
+}
+console.log(JSON.stringify(exportModule(data)));
+module.exports = exportModule;
