5271e85f7f3c4fca040cc362056e9b800c81caf6
Cassidy King <cassidyking17@gmail.com>
2020-06-17 10:25:47 -0400

    pushed returned data types onto an array

--- GetDataType.js
+++ GetDataType.js
@@ -1,19 +1,18 @@
 const data = require('./response.json');
+const dataTypes = [];
 function loadData(e) {
-
-      
-      console.log(data);
-      const deff = data.library.statements.def;
-      const filtered = deff.filter(defined);
-      for (let i = 0; i < filtered.length; i++) {
-        console.log(filtered[i].expression.operand.dataType);
-      }
-      function defined(set) {
-          for (let j = 0; j < deff.length; j++) {
-            if (set.expression.operand !== undefined) return set;
-          }
-      }
-    
+  const deff = data.library.statements.def;
+  const filtered = deff.filter(defined);
+  for (let i = 0; i < filtered.length; i++) {
+    let dataType=filtered[i].expression.operand.dataType;
+    dataTypes[i]=dataType;
+  }
+  function defined(set) {
+    for (let j = 0; j < deff.length; j++) {
+      if (set.expression.operand !== undefined) return set;
     }
-  loadData();
-  
+  }
+  return dataTypes;
+}
+
+module.exports = loadData;
