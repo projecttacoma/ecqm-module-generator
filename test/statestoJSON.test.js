1abe761e1a14644301e0a5fbd6b4f6fdfbeb11db
Cassidy King <cassidyking17@gmail.com>
2020-06-23 08:14:29 -0400

    changed for loop to use object.entries

--- test/statestoJSON.test.js
+++ test/statestoJSON.test.js
@@ -1,7 +1,8 @@
 const factory = require('../factory.js');
 const JSONMap = require('../toJSONMap.js');
-for(var classType in JSONMap){
-    test(`toJSON method returns an correct representation of a toJSON string for the specified class`, () => {
-        expect(factory(classType).toJSON()).toMatchObject(JSONMap[classType]);
-    });
-}
+
+Object.entries(JSONMap).forEach(([dataType, stateJSON]) => {
+  test('toJSON method returns an correct representation of a toJSON string for the specified class', () => {
+    expect(factory(dataType).toJSON()).toMatchObject(stateJSON);
+  });
+});
