97e8f3f23cfbf77c76361d4ffc478b694d9e6fd3
Cassidy King <cassidyking17@gmail.com>
2020-06-23 08:15:21 -0400

    changed for loop to use object.entries

--- test/factory.test.js
+++ test/factory.test.js
@@ -1,8 +1,10 @@
 const factory = require('../factory.js');
 const STATE_LOOKUP = require('../stateMap.js');
 
-for(var dataType in STATE_LOOKUP){
-    test(`factory returns an instance of the correct class when the ${dataType.substring(21)} data type is inputted`, () => {
-        expect(factory(dataType)).toBeInstanceOf(STATE_LOOKUP[dataType]);
-    });
-}
+Object.entries(STATE_LOOKUP).forEach(([dataType, stateClass]) => {
+  test(`factory returns an instance of the correct class when the ${dataType.substring(
+    21
+  )} data type is inputted`, () => {
+    expect(factory(dataType)).toBeInstanceOf(stateClass);
+  });
+});
