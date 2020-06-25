fc3bd65c40e8aed385904a3c5cb58f2ce5762352
Cassidy King <cassidyking17@gmail.com>
2020-06-25 13:45:31 -0400

    fixed error while assigning all series/codes to empty arrays

--- factory.js
+++ factory.js
@@ -4,25 +4,25 @@ function factory(input) {
   let newState;
   switch (input) {
     case '{http://hl7.org/fhir}Encounter':
-      newState = new states.EncounterState('Encounter', 'encounter_class', ['codes']);
+      newState = new states.EncounterState('Encounter', 'encounter_class', []);
       break;
     case '{http://hl7.org/fhir}Condition':
-      newState = new states.ConditionOnsetState('Condition', 'target_encounter', ['codes']);
+      newState = new states.ConditionOnsetState('Condition', 'target_encounter', []);
       break;
     case '{http://hl7.org/fhir}AllergyIntolerance':
-      newState = new states.AllergyOnsetState('AllergyIntolerance', 'target_encounter', ['codes']);
+      newState = new states.AllergyOnsetState('AllergyIntolerance', 'target_encounter', []);
       break;
     case '{http://hl7.org/fhir}Medication':
-      newState = new states.MedicationOrderState('Medication', ['codes']);
+      newState = new states.MedicationOrderState('Medication', []);
       break;
     case '{http://hl7.org/fhir}CarePlan':
-      newState = new states.CarePlanStartState('CarePlan', ['codes']);
+      newState = new states.CarePlanStartState('CarePlan', []);
       break;
     case '{http://hl7.org/fhir}Procedure':
-      newState = new states.ProcedureState('Procedure', ['codes']);
+      newState = new states.ProcedureState('Procedure', []);
       break;
     case '{http://hl7.org/fhir}ImagingStudy':
-      newState = new states.ImagingStudyState('ImagingStudy', 'procedure_code', ['series']);
+      newState = new states.ImagingStudyState('ImagingStudy', 'procedure_code', []);
       break;
     case '{http://hl7.org/fhir}Device':
       newState = new states.DeviceState('Device', 'code');
@@ -31,10 +31,10 @@ function factory(input) {
       newState = new states.SupplyListState('SupplyDelivery', 'supplies');
       break;
     case '{http://hl7.org/fhir}Observation':
-      newState = new states.ObservationState('Observation', 'category', 'unit', ['codes']);
+      newState = new states.ObservationState('Observation', 'category', 'unit', []);
       break;
     case '{http://hl7.org/fhir}DiagnosticReport':
-      newState = new states.DiagnosticReportState('DiagnosticReport', 'number_of_observations', ['codes']);
+      newState = new states.DiagnosticReportState('DiagnosticReport', 'number_of_observations', []);
       break;
     default:
       newState = null;
