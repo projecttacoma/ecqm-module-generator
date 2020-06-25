df36f5b2f0d4403d931b8248a8435638837007a6
Cassidy King <cassidyking17@gmail.com>
2020-06-25 10:58:48 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {},
+};
