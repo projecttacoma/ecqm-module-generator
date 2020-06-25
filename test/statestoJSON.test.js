2beb8e8e108fcdd57b9ff8400e171ba961cd5cb3
Cassidy King <cassidyking17@gmail.com>
2020-06-25 13:34:41 -0400

    changed toMatchObject to toEqual

--- test/statestoJSON.test.js
+++ test/statestoJSON.test.js
@@ -1,5 +1,5 @@
 const factory = require('../factory.js');
-const JSONMap = require('../toJSONMap.js');
+const JSONMap = require('./toJSONMap.js');
 
 Object.entries(JSONMap).forEach(([dataType, stateJSON]) => {
   test('toJSON method returns an correct representation of a toJSON string for the specified class', () => {
