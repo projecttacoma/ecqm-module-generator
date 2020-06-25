9d75e7ef6855d5ca4eead9fed0edc988854dbc4b
Cassidy King <cassidyking17@gmail.com>
2020-06-25 13:01:00 -0400

    updated version of exportModule.js to incorporate the CLI

--- exportModule.js
+++ exportModule.js
@@ -1,29 +1,24 @@
-const fs = require('fs');
-const data = require('./response.json');
 const loadData = require('./GetDataType.js');
 const states = require('./states.js');
 const factory = require('./factory.js');
 
-function exportModule() {
-  const dataTypes = loadData();
+function exportModule(data) {
+  const dataTypes = loadData(data);
   const moduleJSON = {
     name: data.library.identifier.id,
     remarks: [],
     states: {
       Initial: new states.InitialState('Initial').toJSON(),
-      Terminal: new states.TerminalState().toJSON(),
+      Terminal: new states.TerminalState('Terminal').toJSON(),
     },
   };
-  dataTypes.forEach(function (temp, i) {
-    try {
-      const stateName = `${temp.substring(21)}_${i}`;
+  dataTypes.forEach((temp, i) => {
+    const stateName = `${temp.substring(21)}_${i}`;
+    const StateClass = factory(temp);
+    if (StateClass !== null) {
       moduleJSON.states[stateName] = factory(temp).toJSON();
-    } catch {
-      return;
     }
   });
-  return JSON.stringify(moduleJSON);
+  return moduleJSON;
 }
-
-fs.writeFileSync(`${data.library.identifier.id}.json`, exportModule(data));
 module.exports = exportModule;
