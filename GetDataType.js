a53e37ca06d9d4eb207316a48de8239caa44b27e
Cassidy King <cassidyking17@gmail.com>
2020-07-01 13:57:34 -0400

    added recursive method to handle levels of conditional statements

--- GetDataType.js
+++ GetDataType.js
@@ -1,13 +1,31 @@
-const data = require('./response.json');
-
 const dataTypes = [];
-function loadData() {
+function loadData(data) {
   const deff = data.library.statements.def;
   const filtered = deff.filter((d) => {
     return d.expression.operand !== undefined;
   });
+  function recursion(array) {
+    if (array.operand === undefined) {
+      for (let h = 0; h < array.length; h += 1) {
+        if (array[h].operand.dataType === undefined) {
+          recursion(array[h].operand);
+        } else dataTypes.push(array[h].operand.dataType);
+      }
+    }
+    return array.dataType;
+  }
+  let section;
   for (let i = 0; i < filtered.length; i += 1) {
-    dataTypes.push(filtered[i].expression.operand.dataType);
+    section = filtered[i].expression.operand;
+    if (section.dataType !== undefined) {
+      dataTypes.push(section.dataType);
+    } else {
+      section.forEach((o) => {
+        if (recursion(o.operand) !== undefined) {
+          dataTypes.push(recursion(o.operand));
+        }
+      });
+    }
   }
 
   return dataTypes;
