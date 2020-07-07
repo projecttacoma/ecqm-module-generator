a241db182651781dcdff62fc642c89192d59cffe
cassidyking17 <49921199+cassidyking17@users.noreply.github.com>
2020-07-07 16:18:39 -0400

    added recursive method to handle levels of conditional statements (#6)
    * added recursive method to handle levels of conditional statements
    
    * bringing cli.js to Conditionals branch
    
    * test file for GetDataType.js
    
    * files to run through GetDataType.js to test it
    
    * added recursive method to handle levels of conditional statements
    
    * no new changes
    
    * travis file
    
    * travis file without npm run build
    
    * renamed method "recursion" to "traverseELM"
    
    * made traverseELM(o.operand) a variable

--- GetDataType.js
+++ GetDataType.js
@@ -1,11 +1,33 @@
-const dataTypes = [];
 function loadData(data) {
+  const dataTypes = [];
   const deff = data.library.statements.def;
+
   const filtered = deff.filter((d) => {
     return d.expression.operand !== undefined;
   });
+  function traverseELM(array) {
+    if (array.operand === undefined) {
+      for (let h = 0; h < array.length; h += 1) {
+        if (array[h].operand.dataType === undefined) {
+          traverseELM(array[h].operand);
+        } else dataTypes.push(array[h].operand.dataType);
+      }
+    }
+    return array.dataType;
+  }
+  let section;
   for (let i = 0; i < filtered.length; i += 1) {
-    dataTypes.push(filtered[i].expression.operand.dataType);
+    section = filtered[i].expression.operand;
+    if (section.dataType !== undefined) {
+      dataTypes.push(section.dataType);
+    } else {
+      section.forEach((o) => {
+        const returnResult = traverseELM(o.operand);
+        if (returnResult !== undefined) {
+          dataTypes.push(returnResult);
+        }
+      });
+    }
   }
 
   return dataTypes;
