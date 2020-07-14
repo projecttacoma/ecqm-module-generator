a27fa2d7d90e4f5b1c75cbbc4c9b6ceeb84d07ec
Cassidy King <cassidyking17@gmail.com>
2020-07-14 13:13:12 -0400

    made changes to incoporate inclusion of the valueSet in codes

--- test/toJSONMap.js
+++ test/toJSONMap.js
@@ -2,35 +2,35 @@ const JSONMap = {
   '{http://hl7.org/fhir}Encounter': {
     type: 'Encounter',
     direct_transition: 'Encounter',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
     encounter_class: 'encounter_class',
   },
   '{http://hl7.org/fhir}Condition': {
     type: 'ConditionOnset',
     direct_transition: 'Condition',
     target_encounter: 'target_encounter',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
   },
   '{http://hl7.org/fhir}AllergyIntolerance': {
     type: 'AllergyOnset',
     direct_transition: 'AllergyIntolerance',
     target_encounter: 'target_encounter',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
   },
   '{http://hl7.org/fhir}Medication': {
     type: 'MedicationOrder',
     direct_transition: 'Medication',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
   },
   '{http://hl7.org/fhir}CarePlan': {
     type: 'CarePlanStart',
     direct_transition: 'CarePlan',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
   },
   '{http://hl7.org/fhir}Procedure': {
     type: 'Procedure',
     direct_transition: 'Procedure',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
     duration: undefined,
   },
   '{http://hl7.org/fhir}ImagingStudy': {
@@ -54,13 +54,13 @@ const JSONMap = {
     direct_transition: 'Observation',
     category: 'category',
     unit: 'unit',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
   },
   '{http://hl7.org/fhir}DiagnosticReport': {
     type: 'DiagnosticReport',
     direct_transition: 'DiagnosticReport',
     number_of_observations: 'number_of_observations',
-    codes: [],
+    codes: [{ code: '', system: '', display: '', value_set: 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1020' }],
   },
 };
 
