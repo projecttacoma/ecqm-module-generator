1c7cde644cadc6a5e77db6bb86a47d91cf61a728
Cassidy King <cassidyking17@gmail.com>
2020-07-14 13:08:08 -0400

    changed test to incorporate the changes made to GetDataType.js

--- test/GetDataType.test.js
+++ test/GetDataType.test.js
@@ -3,18 +3,61 @@ const testJSON = require('./testJSON.json');
 const test2JSON = require('./test2JSON.json');
 const test3JSON = require('./test3JSON.json');
 
-const results1 = ['{http://hl7.org/fhir}Patient', '{http://hl7.org/fhir}Encounter', '{http://hl7.org/fhir}Procedure'];
-const results2 = ['{http://hl7.org/fhir}Patient', '{http://hl7.org/fhir}Procedure', '{http://hl7.org/fhir}Encounter'];
+const results1 = [
+  { dataType: '{http://hl7.org/fhir}Patient', type: 'Retrieve' },
+  {
+    dataType: '{http://hl7.org/fhir}Encounter',
+    codeProperty: 'code',
+    type: 'Retrieve',
+    codes: { name: 'Colonoscopy', type: 'ValueSetRef' }
+  },
+  {
+    dataType: '{http://hl7.org/fhir}Procedure',
+    codeProperty: 'code',
+    type: 'Retrieve',
+    codes: { name: 'Colonoscopy', type: 'ValueSetRef' }
+  }
+];
+const results2 = [
+  { dataType: '{http://hl7.org/fhir}Patient', type: 'Retrieve' },
+  {
+    dataType: '{http://hl7.org/fhir}Procedure',
+    codeProperty: 'code',
+    type: 'Retrieve',
+    codes: { name: 'Colonoscopy', type: 'ValueSetRef' }
+  },
+  {
+    dataType: '{http://hl7.org/fhir}Encounter',
+    codeProperty: 'type',
+    type: 'Retrieve',
+    codes: { name: 'Colonoscopy', type: 'ValueSetRef' }
+  }
+]
 const results3 = [
-  '{http://hl7.org/fhir}Patient',
-  '{http://hl7.org/fhir}Procedure',
-  '{http://hl7.org/fhir}Encounter',
-  '{http://hl7.org/fhir}Medication',
+  { dataType: '{http://hl7.org/fhir}Patient', type: 'Retrieve' },
+  {
+    dataType: '{http://hl7.org/fhir}Procedure',
+    codeProperty: 'code',
+    type: 'Retrieve',
+    codes: { name: 'Colonoscopy', type: 'ValueSetRef' }
+  },
+  {
+    dataType: '{http://hl7.org/fhir}Encounter',
+    codeProperty: 'type',
+    type: 'Retrieve',
+    codes: { name: 'Colonoscopy', type: 'ValueSetRef' }
+  },
+  {
+    dataType: '{http://hl7.org/fhir}Medication',
+    codeProperty: 'medication',
+    type: 'Retrieve',
+    codes: { name: 'Colonoscopy', type: 'ValueSetRef' }
+  }
 ];
 
 test(`GetDataType.js returns the correct data types for testJSON file`, () => {
   expect(loadData(testJSON)).toEqual(results1);
-});
+ });
 test(`GetDataType.js returns the correct data types for test2JSON file`, () => {
   expect(loadData(test2JSON)).toEqual(results2);
 });
