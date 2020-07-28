cb70380fe50d4b0d9a96eb497134dd8264794b40
Cassidy King <cassidyking17@gmail.com>
2020-07-28 13:50:38 -0400

    folder of helpers

---
+++ src/helpers/GetValueSet.js
@@ -0,0 +1,17 @@
+function loadValueSet(data) {
+  const valueSetMap = {};
+  function getValueSet(file) {
+    file.library.valueSets.def.forEach((set) => {
+      valueSetMap[set.name] = set.id;
+    });
+  }
+  if (data.mainLibrary.library.valueSets !== undefined) {
+    getValueSet(data.mainLibrary);
+  } else if (data.dependencies !== undefined) {
+    data.dependencies.forEach((dependency) => {
+      getValueSet(dependency);
+    });
+  }
+  return valueSetMap;
+}
+module.exports = loadValueSet;
