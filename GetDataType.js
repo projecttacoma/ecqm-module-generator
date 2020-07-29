c79e4a9ede205404e2f971cd776fa2158edfc911
Cassidy King <cassidyking17@gmail.com>
2020-07-29 15:51:58 -0400

    changed default arrow function return to be null

--- GetDataType.js
+++ GetDataType.js
@@ -4,9 +4,9 @@ const { mapDeep } = require('deepdash')(_);
 function loadData(data) {
   const expressions = data.mainLibrary.library.statements.def.map((e) => e.expression);
   const dataTypes = _.compact(
-    // eslint-disable-next-line consistent-return
     mapDeep(expressions, (value, key) => {
       if (value.dataType && (key === 'expression' || key === 'operand')) return value;
+      return null;
     })
   );
   return dataTypes;
