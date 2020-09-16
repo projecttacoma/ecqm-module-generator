78ee28be1d0b367d7befc288a0bddc6c2d5cbf1d
Cassidy King <cassidyking17@gmail.com>
2020-09-16 18:43:26 -0400

    changed so cli takes in bundle and gets mainFile from that

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 const { Command } = require('commander');
+const { Base64 } = require('js-base64');
 const fs = require('fs');
 const path = require('path');
 const exportModule = require('./exportModule');
@@ -9,33 +10,28 @@ program.version('0.0.1');
 function loadELM() {
   program
     .version('0.0.1')
-    .requiredOption('--elmJSON <file>', 'elm json to turn to synthea module')
-    .option('--dependency <library>', 'directory of dependent libraries')
+    .requiredOption('--elmJSONBundle <bundle>', 'elm json to turn to synthea module')
     .parse(process.argv);
+
   const ELMFiles = {};
   try {
-    const mainFile = fs.readFileSync(program.elmJSON, 'utf8');
+    const bundle = JSON.parse(fs.readFileSync(program.elmJSONBundle, 'utf8'));
+    logger.info(`Sucessfully loaded in bundle with id: ${bundle.id}`);
+    const measure = bundle.entry.find(({ resource }) => resource.resourceType === 'Measure').resource.library[0];
+    const ID = measure.substring(measure.indexOf('/') + 1);
+    logger.info(`finding main library with id: ${ID}`);
+    const dataResource = bundle.entry.find(({ resource }) => resource.id === ID);
+    const encodedData = JSON.stringify(
+      dataResource.resource.content.find((c) => c.contentType === 'application/elm+json').data
+    );
+    logger.info(`decoding data...`);
+    Base64.extendString();
+    const mainFile = Base64.decode(encodedData);
+    logger.info(`data decoded successfully and added to mainFile variable`);
     ELMFiles.mainLibrary = JSON.parse(mainFile);
   } catch (err) {
     logger.error(err.message);
     process.exit(1);
-  }
-  if (program.dependency !== undefined) {
-    const directoryPath = path.resolve(program.dependency, './');
-    ELMFiles.dependencies = [];
-    const dependentFiles = fs.readdirSync(directoryPath).filter((file) => {
-      return file.split('.').pop() === 'json';
-    });
-    try {
-      dependentFiles.forEach((file) => {
-        logger.info(`parsing ${file}`);
-        const dependency = fs.readFileSync(`${directoryPath}/${file}`, 'utf8');
-        ELMFiles.dependencies.push(JSON.parse(dependency));
-      });
-    } catch (err) {
-      logger.error(err.message);
-      process.exit(1);
-    }
   }
   return ELMFiles;
 }
