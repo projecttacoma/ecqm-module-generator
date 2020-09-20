bf961f2444615f6c5ec23fcd904f272a1df559a2
Cassidy King <cassidyking17@gmail.com>
2020-09-20 19:35:26 -0400

    loads dependency data from the bundle

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 const { Command } = require('commander');
 const { Base64 } = require('js-base64');
 const fs = require('fs');
-const path = require('path');
 const exportModule = require('./exportModule');
 const logger = require('./helpers/logger');
 
@@ -17,18 +16,35 @@ function loadELM() {
   try {
     const bundle = JSON.parse(fs.readFileSync(program.elmJSONBundle, 'utf8'));
     logger.info(`Sucessfully loaded in bundle with id: ${bundle.id}`);
-    const measure = bundle.entry.find(({ resource }) => resource.resourceType === 'Measure').resource.library[0];
-    const ID = measure.substring(measure.indexOf('/') + 1);
-    logger.info(`finding main library with id: ${ID}`);
+    const measure = bundle.entry.find(({ resource }) => resource.resourceType === 'Measure');
+    const measureID = measure.resource.library[0];
+    const ID = measureID.substring(measureID.indexOf('/') + 1);
+    logger.info(`finding main library with ID: ${ID}`);
     const dataResource = bundle.entry.find(({ resource }) => resource.id === ID);
-    const encodedData = JSON.stringify(
-      dataResource.resource.content.find((c) => c.contentType === 'application/elm+json').data
-    );
-    logger.info(`decoding data...`);
-    Base64.extendString();
+    const encodedData = dataResource.resource.content.find((c) => c.contentType === 'application/elm+json').data;
     const mainFile = Base64.decode(encodedData);
-    logger.info(`data decoded successfully and added to mainFile variable`);
     ELMFiles.mainLibrary = JSON.parse(mainFile);
+    const dependencies = measure.resource.relatedArtifact.filter((d) => {
+      return d.type === 'depends-on';
+    });
+    try {
+      ELMFiles.dependencies = [];
+      const dependenciesID = [];
+      dependencies.forEach((dd) => {
+        if (dd.resource !== undefined) dependenciesID.push(dd.resource.substring(measureID.indexOf('/') + 1));
+      });
+      logger.info(`finding dependencies with IDs: ${dependenciesID}`);
+      dependenciesID.forEach((dependencyID) => {
+        const dataResource1 = bundle.entry.find(({ resource }) => resource.id === dependencyID);
+        const encodedData1 = dataResource1.resource.content.find((c) => c.contentType === 'application/elm+json').data;
+        const dependency = Base64.decode(encodedData1);
+        ELMFiles.dependencies.push(JSON.parse(dependency));
+      });
+      logger.info(`adding dependencies to ELMFiles`);
+    } catch (err) {
+      logger.error(err.message);
+      process.exit(1);
+    }
   } catch (err) {
     logger.error(err.message);
     process.exit(1);
