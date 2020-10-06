bf879dd711123a44e2f5443fd73ce841e4dabab2
cassidyking17 <49921199+cassidyking17@users.noreply.github.com>
2020-10-06 15:52:28 -0400

    Adjusted error handling (#24)
    * adjusted error handling
    
    * changed console.log to logger.error
    
    Co-authored-by: Matthew Gramigna <mgramigna@mitre.org>
    
    * adding err.message to the error throw message
    
    Co-authored-by: Matthew Gramigna <mgramigna@mitre.org>
    
    Co-authored-by: Matthew Gramigna <mgramigna@mitre.org>

--- src/cli.js
+++ src/cli.js
@@ -12,7 +12,13 @@ program
 const bundle = JSON.parse(fs.readFileSync(program.bundle, 'utf8'));
 // false for logs, true for no logs
 const generator = new Generator(bundle, false);
-const moduleJSON = generator.generate();
+let moduleJSON;
+try {
+  moduleJSON = generator.generate();
+} catch (err) {
+  logger.error(err.message);
+  process.exit(1);
+}
 
 logger.info(`exporting file with name: ${moduleJSON.name}.json`);
 fs.writeFileSync(`${moduleJSON.name}.json`, JSON.stringify(moduleJSON));
