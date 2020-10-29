675974fde0959ac0a10b52405867e428919d299f
Cassidy King <cassidyking17@gmail.com>
2020-10-29 15:58:32 -0400

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'ecqm-module-generator',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+};
